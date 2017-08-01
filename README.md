e1c70a972c6bfc432bcaa844c1100a708960d900
shaminaus@gamil.com <shaminaus@gmail.com>
2017-07-31 22:14:01 -0500

    Setting webpack, babel, index.js and build

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# star-js
+:star: # star-js
 ## Just another application
 
 # Setup
-```
+```shell
 git clone https://github.com/MuhammadShamim/star-js.git star-js
 cd star-js
+npm install
+npm run dev
 ```
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+module.exports = env => {
+	return {
+		entry: './src/index.js',
+		output: {
+			path: path.resolve(__dirname, 'build'),
+			filename: env === "dev" ? 'star.js': "star-min.js"
+		},
+		module: {
+			loaders: [
+				{
+					loader: 'babel-loader',
+					test: /\.js$/,
+					exclude: /node_modules/,
+					query: {
+						presets: ['es2015']
+					}
+				}
+			]
+		},
+		devServer: {
+			port: 786,
+			contentBase: './build',
+			inline: true
+		}
+    }
+}
